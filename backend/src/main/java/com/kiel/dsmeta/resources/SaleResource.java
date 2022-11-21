f7a4eb1f7227f1793696f90f5867325d607c9c6f
kiel025 <kiel02505@gmail.com>
2022-11-21 11:57:44 -0300

    Date select

--- backend/src/main/java/com/kiel/dsmeta/resources/SaleResource.java
+++ backend/src/main/java/com/kiel/dsmeta/resources/SaleResource.java
@@ -3,12 +3,15 @@ package com.kiel.dsmeta.resources;
 import com.kiel.dsmeta.entities.Sale;
 import com.kiel.dsmeta.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -18,8 +21,10 @@ public class SaleResource {
     private SaleService service;
 
     @GetMapping
-    public ResponseEntity<List<Sale>> findAll() {
-        List<Sale> sales = service.findAll();
+    public ResponseEntity<Page<Sale>> findSales(@RequestParam(value = "minDate", defaultValue = "") String minDate,
+                                                @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+                                                Pageable pageable) {
+        Page<Sale> sales = service.findSales(minDate, maxDate, pageable);
         return ResponseEntity.ok().body(sales);
     }
 
--- backend/src/main/java/com/kiel/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/kiel/dsmeta/services/SaleService.java
@@ -3,9 +3,13 @@ package com.kiel.dsmeta.services;
 import com.kiel.dsmeta.entities.Sale;
 import com.kiel.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SaleService {
@@ -13,8 +17,15 @@ public class SaleService {
     @Autowired
     private SaleRepository repository;
 
-    public List<Sale> findAll() {
-        return repository.findAll();
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+        LocalDate oneYearAgo = today.minusYears(1);
+
+        LocalDate min = minDate.equals("") ? oneYearAgo : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+        return repository.findSales(min, max, pageable);
     }
 
 }
