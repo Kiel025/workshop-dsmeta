b30ca31bb6367cfe691d0c21e1c3283fc90b7c0d
kiel025 <kiel02505@gmail.com>
2022-11-21 14:10:17 -0300

    Twilio SMS

--- backend/src/main/java/com/kiel/dsmeta/resources/SaleResource.java
+++ backend/src/main/java/com/kiel/dsmeta/resources/SaleResource.java
@@ -2,14 +2,13 @@ package com.kiel.dsmeta.resources;
 
 import com.kiel.dsmeta.entities.Sale;
 import com.kiel.dsmeta.services.SaleService;
+import com.kiel.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.function.EntityResponse;
 
 import java.time.LocalDate;
 
@@ -20,12 +19,21 @@ public class SaleResource {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public ResponseEntity<Page<Sale>> findSales(@RequestParam(value = "minDate", defaultValue = "") String minDate,
                                                 @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
                                                 Pageable pageable) {
         Page<Sale> sales = service.findSales(minDate, maxDate, pageable);
         return ResponseEntity.ok().body(sales);
+    }
+
+    @GetMapping("/{id}/notification")
+    public ResponseEntity<Void> notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
